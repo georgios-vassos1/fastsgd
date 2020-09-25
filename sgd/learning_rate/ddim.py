1137ce82c4a45c3d2204c14c4d3d0cab60558622
georgios.vassos1 <georgios.vassos1@maersk.com>
2020-09-25 19:17:09 +0200

    renamed directory to replace '-' with '_'

---
+++ sgd/learning_rate/ddim.py
@@ -0,0 +1,10 @@
+from base import BaseLR
+from value import LRvalue
+import numpy as np
+
+class ddimLR:
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
---
+++ sgd/learning_rate/onedim.py
@@ -0,0 +1,10 @@
+from base import BaseLR
+from value import LRvalue
+import numpy as np
+
+class OnedimLR:
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
