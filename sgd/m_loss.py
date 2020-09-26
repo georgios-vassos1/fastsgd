c309a8d3f416a8271948340c4f0ceab666de5329
georgios.vassos1 <georgios.vassos1@maersk.com>
2020-09-26 12:55:11 +0200

    small fix plus partial vectorizer

--- sgd/m_loss.py
+++ sgd/m_loss.py
@@ -1,29 +1,26 @@
 import numpy as np
+from functools import partial
 
 class BaseLoss:
     def __init__(self):
         pass
 
-    def loss(self, u: float, l: float=None) -> float:
-        return 0.0
+    def loss(self, u: float, l: float=0.0) -> float: return
 
-    def first_deriv(self, u: float, l: float=None) -> float:
-        return 0.0
+    def first_deriv(self, u: float, l: float=0.0) -> float: return
 
-    def second_deriv(self, u: float, l: float=None) -> float:
-        return 0.0
+    def second_deriv(self, u: float, l: float=0.0) -> float: return
 
-    def third_deriv(self, u: float, l: float=None) -> float:
-        return 0.0
+    def third_deriv(self, u: float, l: float=0.0) -> float: return
 
-    def loss_vec(self, u: pd.ndarray, l: float=None) -> np.ndarray:
-        return np.vectorize(self.loss)(u, l)
+    def loss_vec(self, u: np.ndarray, l: float) -> np.ndarray:
+        return np.vectorize(partial(self.loss, l=l))(u)
 
-    def first_deriv_vec(self, u: np.ndarray, l: float=None) -> np.ndarray:
-        return np.vectorize(self.first_deriv)(u, l)
+    def first_deriv_vec(self, u: np.ndarray, l: float) -> np.ndarray:
+        return np.vectorize(partial(self.first_deriv, l=l))(u)
 
-    def second_deriv_vec(self, u: np.ndarray, l: float=None) -> np.ndarray:
-        return np.vectorize(self.second_deriv)(u, l)
+    def second_deriv_vec(self, u: np.ndarray, l: float) -> np.ndarray:
+        return np.vectorize(partial(self.second_deriv, l=l))(u)
 
 
 class HuberLoss(BaseLoss):
@@ -31,22 +28,21 @@ class HuberLoss(BaseLoss):
         pass
 
     def loss(self, u: float, l: float) -> float:
-        if np.abs(u) <= l:
-            return np.power(u, 2) >> 1
-        else:
-            return (l * np.abs(u)) - (np.power(u, 2) >> 1)
+        if np.abs(u) <= l: return np.power(u, 2) / 2
+        else: return (l * np.abs(u)) - (np.power(u, 2) / 2)
 
     def first_deriv(self, u: float, l: float) -> float:
-        if np.abs(u) <= l:
-            return u
-        else:
-            return l * np.sign(u)
+        if np.abs(u) <= l: return u
+        else: return l * np.sign(u)
 
     def second_deriv(self, u: float, l: float) -> float:
-        if np.abs(u) <= l:
-            return 1.0
-        else:
-            return 0.0
+        if np.abs(u) <= l: return 1.0
+        else: return 0.0
 
     def third_deriv(self, u: float, l: float) -> float:
         return 0.0
+
+# if __name__=='__main__':
+#     hl = HuberLoss()
+#     print(hl.loss(10.0, 5.0), hl.first_deriv(10.0, 5.0))
+#     print(hl.loss_vec(np.arange(1,11), 5.0), hl.first_deriv_vec(np.arange(1,11), 5.0))
