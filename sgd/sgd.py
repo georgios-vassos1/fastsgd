ccd4068ccfbd7301f12b8d291cf4beb701ac2eb9
georgios.vassos1 <georgios.vassos1@maersk.com>
2020-10-13 15:36:15 +0200

    Correct kwargs usage plus cleaning up

--- sgd/sgd.py
+++ sgd/sgd.py
@@ -7,12 +7,12 @@ from .learning_rate.__init__ import *
 
 
 class SGD:
-    def __init__(self, n_samples: int, timer: time, **details):
-        self._name = details["method"]
-        self._n_params = details["nparams"]
-        self._reltol = details["reltol"]    # relative tolerance for convergence
-        self._n_passes = details["npasses"] # number of passes over data
-        self._size = details["size"]        # number of estimates to be recorded (log-uniformly)
+    def __init__(self, n: int, p: int, timer: time, **kwargs):
+        self._name = kwargs.get("method", None)
+        self._n_params = p
+        self._reltol = kwargs.get("reltol", 1e-5)   # relative tolerance for convergence
+        self._n_passes = kwargs.get("npasses", 10)  # number of passes over data
+        self._size = kwargs.get("size", 10)        # number of estimates to be recorded (log-uniformly)
         self._estimates = np.zeros((self._n_params, self._size))
         self._last_estimate = np.zeros(self._n_params)
         self._times = np.zeros(self._size)
@@ -20,19 +20,18 @@ class SGD:
         self._t = 0
         self._n_recorded = 0              # number of coefs that have been recorded
         self._pos = np.zeros(self._size)  # the iteration of recorded coefs
-        self._pass = details["pass"]      # force running for n_passes on data
-        self._check = details["check"]
-
-        if self._check: self._truth = details["truth"]
+        self._pass = kwargs.get("pass", True) # force running for n_passes on data
+        self._check = kwargs.get("check", False)
+        if self._check: self._truth = kwargs.get("truth", None)
 
         ## Select the iterations to store estimates
-        n_iters = n_samples * self._n_passes
+        n_iters = n * self._n_passes
         self._pos = (10.0 ** (np.arange(self._size) * np.log10(float(n_iters)) / (self._size-1))).astype(int)
         if self._pos[-1] != n_iters: self._pos[-1] = n_iters
 
         ## Set learning rate
-        self._lr_choice = details["lr"]     # type of learning rate: 'one-dim', 'one-dim-eigen', 'd-dim', 'adagrad', 'rmsprop'
-        controls = details["lr_controls"]
+        self._lr_choice = kwargs.get("lr", "one-dim")   # type of learning rate: 'one-dim', 'one-dim-eigen', 'd-dim', 'adagrad', 'rmsprop'
+        controls = kwargs.get("lr_controls", {"scale": 1.0, "alpha": 1.0, "gamma": 0.6, "c": 0.5})
         if self._lr_choice == "one-dim":
             self._lr_obj = OnedimLR(controls["scale"], controls["gamma"], controls["alpha"], controls["c"])
         elif self._lr_choice == "one-dim-eigen":
@@ -42,7 +41,7 @@ class SGD:
         elif self._lr_choice == "adagrad":
             self._lr_obj = ddimLR(self._n_params, controls["eta"], 1.0, 1.0, 0.5, controls["eps"])
         elif self._lr_choice == "rmsprop":
-            self._lr_obj = ddimLR(self._n_params, controls["eta"], controls["a"], 1.0 - controls["a"], 0.5, controls["eps"])
+            self._lr_obj = ddimLR(self._n_params, controls["eta"], controls["gamma"], 1.0 - controls["gamma"], 0.5, controls["eps"])
 
 
     def get_value_of(self, attribute: str):
@@ -54,8 +53,8 @@ class SGD:
             qe = np.mean((theta_new - self._truth) ** 2)
             # print(qe)
             if qe < self._reltol: return True
-        elif not self._pass:
-            qe = np.mean(np.mean(np.abs(theta_new - theta_old))) / np.mean(np.mean(np.abs(theta_old)))
+        else:
+            qe = np.mean(np.abs(theta_new - theta_old)) / np.mean(np.abs(theta_old))
             if qe < self._reltol: return True
         return False
 
