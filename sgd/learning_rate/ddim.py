Corrected vectorized computation

sgd/learning_rate/ddim.py
@@ -9,7 +9,7 @@ class ddimLR(BaseLR):
     # rmsprop: a, b, c, eta, eps = gamma, 1-gamma, 1/2, 1, 1e-6
     #
     # d     dimension of learning rate
-    # eta   scale factor in numberator
+    # eta   scale factor in numerator
     # a     weights of perivous gradient
     # b     weight of new gradient
     # c     exponential power
@@ -27,6 +27,7 @@ class ddimLR(BaseLR):
     def __call__(self, t: int, grad_t: np.ndarray) -> LRvalue:
         self.__Idiag = self.__a * self.__Idiag + self.__b * np.power(grad_t, 2)
         mask = np.abs(self.__Idiag) > 1e-8
-        self.__v.lr[mask] = self.__eta / np.power(self.__Idiag + self.__eps, self.__c)
-        if len(self.__v.lr[~mask]): self.__v.lr[~mask] = self.__Idiag[~mask]
+        self.__v.lr[mask] = self.__eta / np.power(self.__Idiag[mask] + self.__eps, self.__c)
+        if np.any(~mask): self.__v.lr[~mask] = self.__Idiag[~mask]
         return self.__v
+